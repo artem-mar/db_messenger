b54bbd39db5559d99cb49e9051018048287f947d
Artem M <artemmarin91@mail.ru>
2023-09-08 02:45:06 +0500

    fix mobile burger

--- src/components/BurgerMenu/MobileBurgerMenu/MobileBurgerMenu.tsx
+++ src/components/BurgerMenu/MobileBurgerMenu/MobileBurgerMenu.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind'
 import { useUIOptions } from 'context'
 import { useState } from 'react'
+import { createPortal } from 'react-dom'
 import { useTranslation } from 'react-i18next'
 import Logo from 'assets/icons/logo.png'
 import { BotInfoInterface } from 'types/types'
@@ -54,34 +55,39 @@ const MobileBurgerMenu = ({ bot }: { bot: BotInfoInterface }) => {
           svgProp={{ className: cx(isOpen && 'rotate') }}
         />
       </div>
-      <div
-        className={cx(s.backdrop, isOpen && 'open')}
-        onClick={() => setIsOpen(false)}
-      >
-        <div className={s.menu}>
-          <MobileBurgerBtn
-            type='dream'
-            linkTo='http://deepdream.builders'
-            name={t('main_burger.about')}
-            handleClick={() => setIsOpen(false)}
-          />
-          <MobileBurgerBtn
-            type='key'
-            name={t('assistant_burger.tokens')}
-            handleClick={handleEnterToken}
-          />
-          <MobileBurgerBtn
-            type='share'
-            name={t('assistant_burger.share')}
-            handleClick={handleShare}
-          />
-          <MobileBurgerBtn
-            type='properties'
-            name={t('assistant_burger.properties')}
-            handleClick={handlePropsOpen}
-          />
-        </div>
-      </div>
+      {createPortal(
+        // Portal is needed to use body as parent component and calculate height in % instead of vh (for chrome mobile )
+        <div
+          className={cx(s.backdrop, isOpen && 'open')}
+          onClick={() => setIsOpen(false)}
+        >
+          <div className={s.menu}>
+            <MobileBurgerBtn
+              type='dream'
+              linkTo='http://deepdream.builders'
+              name={t('main_burger.about')}
+              handleClick={() => setIsOpen(false)}
+            />
+            <MobileBurgerBtn
+              type='key'
+              name={t('assistant_burger.tokens')}
+              handleClick={handleEnterToken}
+            />
+            <MobileBurgerBtn
+              type='share'
+              name={t('assistant_burger.share')}
+              handleClick={handleShare}
+            />
+            <MobileBurgerBtn
+              type='properties'
+              name={t('assistant_burger.properties')}
+              handleClick={handlePropsOpen}
+            />
+          </div>
+          <div className={s.filler}></div>
+        </div>,
+        document.body
+      )}
     </div>
   )
 }
